62f435607fa869b0d2c6fc37f4dc6af674a53600
Surajsuraj87850 <surajsuraj87850@gmail.com>
2022-12-14 19:29:53 +0530

    first commit

---
+++ javaClass/src/day12/exception.java
@@ -0,0 +1,34 @@
+package day12;
+
+public class exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int a =30, b=0;
+			int c=a/b;
+			System.out.println("result ="+c);
+		}
+		catch(ArithmeticException e){
+			System.out.println("cant divide by zero");
+			
+		}
+		try {
+			int num = Integer.parseInt("suraj");
+			System.out.println(num);
+		}
+		catch(NumberFormatException e) {
+			System.out.println("number format exception");
+		}
+		try {
+			int a[]=new int[5];
+			a[7]=9;
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("array index out of bounds");
+			
+		}
+
+	}
+
+}
---
+++ javaClass/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author MALA
+ *
+ */
+module javaClass {
+}
